7e40d7445f30dc7e02f5501511f9627d37bee77b
KotPovar <axchov@gmai.com>
2023-12-04 19:36:40 +0300

    day1

--- src/Day1/Main.java
+++ src/Day1/Main.java
@@ -1,4 +1,4 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+package Day1;// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 import java.io.File;
 import java.io.FileNotFoundException;
